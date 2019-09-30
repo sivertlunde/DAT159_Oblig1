40a0b21323f2d176c57eb6748a857acfc53616d4
sivertlunde <571734@stud.hvl.no>
2019-09-30 10:02:06 +0200

    Generic comment

--- src/dat159/fowler/Customer.java
+++ src/dat159/fowler/Customer.java
@@ -31,13 +31,12 @@ public class Customer {
             		daysRented > 1) frequentRenterPoints ++;
 
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" +
-                    String.valueOf(thisAmount) + "\n";
+            result += "\t" + each.getMovie().getTitle()+ "\t" + thisAmount + "\n";
             totalAmount += thisAmount;
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) +
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints +
                 " frequent renter points";
         return result;
     }
